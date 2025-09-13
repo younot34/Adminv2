2012ddc8199c011b9f6fa6dd610c461ace002705
younot34 <94425967+younot34@users.noreply.github.com>
2025-09-13 13:42:23 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:admin/pages/login_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   if (![
     TargetPlatform.windows,
     TargetPlatform.macOS,
